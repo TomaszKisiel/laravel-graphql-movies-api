3f6791794be9d7750d8db46cc6559727a636cf31
TomaszKisiel <tkisiel5w4@yahoo.com>
2021-05-25 00:30:16 +0200

    Initial commit and migrations.

---
+++ database/migrations/2021_05_24_215824_create_movies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('genre_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->string('title');
+            $table->string('description');
+            $table->integer('duration');
+            $table->datetime('released_at'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
---
+++ database/migrations/2021_05_24_215854_create_actors_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actors', function (Blueprint $table) {
+            $table->id();
+  	    $table->string('first_name');
+	    $table->string('last_name');
+	    $table->enum('gender', ['M', 'F']);
+	    $table->enum('eyes_color', ['blue', 'brown', 'green', 'black', 'gray', 'amber']);
+	    $table->enum('hair_color', ['white', 'silver', 'blonde', 'auburn', 'brunette', 'black', 'ginger']);
+	    $table->float('height', 2, 2);
+            $table->float('weight', 3, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('actors');
+    }
+}
